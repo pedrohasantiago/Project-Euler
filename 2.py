d7d87b1ec9836e39c34ca41299b38b0de43fcc77
Pedro Santiago <pedrohasantiago@gmail.com>
2020-01-04 07:54:36 -0300

    Resolution to problem #2

---
+++ 2.py
@@ -0,0 +1,16 @@
+def sum_even_fibonacci_terms(limit):
+    """Returns the sum of even fibonacci numbers below a given limit.
+    """
+    # Initializing the Fibonacci sequence:
+    second_to_last_term = 1
+    last_term = 1
+    sum = 0
+    # Adding numbers below the limit:
+    while last_term < limit:
+        new_term = last_term + second_to_last_term
+        second_to_last_term = last_term
+        last_term = new_term
+        # Summing even numbers:
+        if new_term % 2 == 0:
+            sum += new_term
+    return sum
