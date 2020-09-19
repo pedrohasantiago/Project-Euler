a2e45562a83b633b91053f23d6d901eb107b2b3f
Pedro Santiago <pedrohasantiago@gmail.com>
2020-09-19 17:00:53 -0300

    Quicker solution 12

--- Solutions/012.py
+++ Solutions/012.py
@@ -3,22 +3,25 @@
 # hundred divisors?
 # (Link: https://projecteuler.net/problem=12)
 
-from math import sqrt
+from math import sqrt, prod
+from functools import lru_cache
 from typing import Iterable
 import numpy as np
 
+FIRST_TRIANGLE_NUMBER = 1
+
 def _generate_triangle_number() -> Iterable[int]:
-    curr = 1
+    curr = FIRST_TRIANGLE_NUMBER
     while True:
         # The n-th triangular number is the sum of all integers until n,
-        # so it is basically a arithmetic projection of ratio 1:
+        # so it is basically an arithmetic projection of ratio 1
         yield curr * (curr + 1) // 2
         curr += 1
 
 def _sieve_of_erastothenes(limit: int) -> np.ndarray:
-    # Taken from https://stackoverflow.com/a/3035188/12058106
+    # Inspired by https://stackoverflow.com/a/3035188/12058106
     if limit < 2:
-        return []
+        return np.array([], dtype=int)
     limit += 1 # Including "limit" itself in the range
     # Initialize half sieve
     is_index_prime = np.ones(limit // 2, dtype=np.bool)
@@ -33,6 +36,7 @@ def _sieve_of_erastothenes(limit: int) -> np.ndarray:
         values=2
     )
 
+@lru_cache
 def _divisor_function(num: int) -> int:
     # Inspired by https://www.geeksforgeeks.org/count-divisors-n-on13/
     divisors = 1 # All numbers have at least 1 divisor (1)
@@ -52,9 +56,23 @@ def _divisor_function(num: int) -> int:
             divisors *= power + 1
     return divisors
 
+def _get_triangle_number_divisors(nth_triangle: int) -> int:
+    """The triangle number is given by (n * (n + 1) // 2)
+    As n and (n + 1) are co-primes, the total number of divisors
+    of a triangle number (D(t)) will be either 
+    (D(n) * D((n + 1) // 2)) or (D(n // 2) * D(n + 1))
+    """
+    if nth_triangle % 2 == 0:
+        first_factor = nth_triangle // 2
+        second_factor = nth_triangle + 1
+    else:
+        first_factor = nth_triangle
+        second_factor = (nth_triangle + 1) // 2
+    return prod(_divisor_function(factor) for factor in (first_factor, second_factor))
+
 def get_triangle_number(more_than_many_divisors: int) -> int:
-    for triangle_num in _generate_triangle_number():
-        if _divisor_function(triangle_num) > more_than_many_divisors:
+    for i, triangle_num in enumerate(_generate_triangle_number(), start=FIRST_TRIANGLE_NUMBER):
+        if _get_triangle_number_divisors(i) > more_than_many_divisors:
             return triangle_num
     assert False, "Unreachable"
 
